64761186b878a5039e814e4b96e72def2d52a609
Adalberto Sampaio <sampaio.afn@gmail.com>
2022-04-16 08:34:33 -0300

    feat: Creates request interceptors

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,30 @@ const api = axios.create({
   baseURL: 'https://fintu-api.herokuapp.com/',
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers = {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      };
+    }
+    return config;
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem('token');
+      window.location = '/';
+    }
+    throw error;
+  }
+);
+
 export default api;
